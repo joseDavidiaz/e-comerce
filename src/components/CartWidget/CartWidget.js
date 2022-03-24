dab23c92bcd3a3a5ad4e11865d4ad854625db45d
David Diaz <josedavid.diaz@outlook.com>
2022-03-24 13:37:39 -0600

    Creccion y correccion

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+
+
+const CartWidget = () => {
+    return (
+        <div>
+            {<img src={"./components/img/carro.png"} />}
+            <p></p>
+        </div>
+    )
+}
+
+export default CartWidget
